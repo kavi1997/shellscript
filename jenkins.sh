eaf6f4380dd56c0bbb1339b5fba00168a37c6818
kavi1997 <37212467+kavi1997@users.noreply.github.com>
2019-12-09 11:37:08 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo  yum update -y
+yum install java-1.8.0
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+yum install -y jenkins
+service jenkins start
+
+echo "initial admin password"
+cat /var/lib/jenkins/secrets/initialAdminPassword
